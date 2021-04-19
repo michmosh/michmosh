d8ea883fde682fd6ae3558e1e19179e269913bc3
michmosh <moshem@aman.co.il>
2021-04-19 15:33:08 +0300

    deploy changes

--- router.js
+++ router.js
@@ -3,24 +3,48 @@ const router = express();
 
 router.get('/', function (req, res) {
     console.log(req)
+    res.status(200)
    res.send('GET /');
 })
 
 router.get('/knowladge-base', function (req, res) {
     const jsonReesponse = require('./mocks/knowladge-base.json')
+    res.status(200)
    res.send(jsonReesponse);
 })
 router.get('/policies', function (req, res) {
     const jsonReesponse = require('./mocks/policies.json')
+    res.status(200)
    res.send(jsonReesponse);
 })
 router.get('/forms', function (req, res) {
     const jsonReesponse = require('./mocks/forms.json')
+    res.status(200)
    res.send(jsonReesponse);
 })
 router.get('/documents', function (req, res) {
     const jsonReesponse = require('./mocks/documents.json')
+    res.status(200)
    res.send(jsonReesponse);
+})
+
+router.post('/policies/:id', async function(){
+    const mockResponse = {
+        status:'ok',
+        success : true
+    }
+    await res.status(200)
+    res.send(mockResponse)
+})
+router.post('/knowladge-base/:id', async function(req,res){
+    const clientId = req.params.id;
+    const mockResponse = {
+        status:'ok',
+        success : true,
+        body:`thank you Max Kozlov - id number :${clientId}`
+    }
+    await res.status(200)
+    res.send(mockResponse)
 })
 
 module.exports = router
